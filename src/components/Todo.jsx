4de64c9489dca7e09f8b2bcd100422adea05edfd
Steopa2001 <moraru495@gmail.com>
2026-02-05 12:52:12 +0100

    [feat] Add todo input and dynamic task list

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,23 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
+import TodoItems from "./TodoItems";
 
 const Todo = () => {
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const add = () => {
+    if (text.trim() === "") return;
+
+    setTodos([...todos, text]);
+    setText("");
+  };
+
   return (
-    <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-137.5 rounded-xl'>
-      
+    <div className="bg-slate-300 place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-137.5 rounded-xl">
       {/* TITLE and IMAGE */}
-      <div className='flex items-center mt-7 gap-2'>
-        <img className='w-8' src="/todo_icon.png" alt="icon-todo" />
-        <h1 className='text-3xl font-semibold'>To-Do List</h1>
+      <div className="flex items-center mt-7 gap-2">
+        <img className="w-8" src="/todo_icon.png" alt="icon-todo" />
+        <h1 className="text-3xl font-semibold">To-Do List</h1>
       </div>
 
       {/* INPUT  */}
-      <div className='flex items-center my-7 bg-gray-200 rounded-full'>
-        <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task...'/>
-        <button className='cursor-pointer border-none rounded-full bg-orange-600 w-28 h-14 text-white text-lg font-medium'>ADD +</button>
+      <div className="flex items-center my-7 bg-gray-200 rounded-full">
+        <input
+          className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          type="text"
+          placeholder="Add your task..."
+        />
+        <button onClick={add} className="cursor-pointer border-none rounded-full bg-orange-600 w-28 h-14 text-white text-lg font-medium">
+          ADD +
+        </button>
       </div>
 
+    {/* TODO LIST */}
+      <div>
+        {todos.map((todo, index) => (
+          <TodoItems key={index} text={todo} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
---
+++ src/components/TodoItems.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const TodoItems = ({text}) => {
+  return (
+    <div className='flex items-center my-3 gap-2'>
+      <div className='flex flex-1 items-center cursor-pointer'>
+        <img className='w-7' src="/tick.png" alt="tick" />
+        <p className='text-slate-700 ml-4 text-[17px]'>{text}</p>
+      </div>
+
+      <img className='w-3.5 cursor-pointer' src="/delete.png" alt="delete-icon" />
+    </div>
+  )
+} 
+
+export default TodoItems
